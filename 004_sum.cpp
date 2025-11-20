5c7471d9df9fa14edc4c9e379befac80f1625912
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-20 23:53:01 +0600

    solve sum

---
+++ 004_sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num1, num2, sum;
+
+    cout<<"Enter 1st Number: ";
+    cin>>num1;
+    cout<<"2nd: ";
+    cin>>num2;
+
+    sum = num1 + num2;
+
+    cout<<sum;
+
+    return 0;
+}
