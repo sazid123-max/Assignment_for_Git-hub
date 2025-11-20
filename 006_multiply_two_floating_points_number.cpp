4fac7017d6e192483e0bed4f94d9f9915743d763
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-21 00:28:45 +0600

    multiply two floating point numbers

---
+++ 006_multiply_two_floating_points_number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float a, b, multiply;
+
+    cout << "Give First Number: ";
+    cin >> a;
+    cout << "Give Second Number: ";
+    cin >> b;
+
+    multiply = a * b;
+
+    cout << "Multiply: "<<multiply;
+
+    return 0;
+}
