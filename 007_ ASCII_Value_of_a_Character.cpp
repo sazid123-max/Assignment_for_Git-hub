ce00d7f86b4cb4635bfdb63d2a5130585a7dae36
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-21 15:31:16 +0600

    solve  ASCII Value of a Character

---
+++ 007_ ASCII_Value_of_a_Character.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    // Taking character input
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    // Printing ASCII value
+    cout << "ASCII value of '" << ch << "' is: " << int(ch);
+
+    return 0;
+}
+
