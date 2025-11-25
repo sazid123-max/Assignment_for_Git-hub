3c9d2c2a8ddbcb736bf62eccb77099fc50a19cdf
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-25 22:03:14 +0600

    solve Print Hollow Star Pyramid

---
+++ 045_Print_Hollow_Star_Pyramid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= rows - i; j++) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            if (j == 1 || j == (2 * i - 1) || i == rows)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
