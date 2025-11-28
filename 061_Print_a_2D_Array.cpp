631deb74e38bec2f4d526819aa13831ae8167e80
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-29 00:46:29 +0600

    solve to Print a 2D Array

---
+++ 061_Print_a_2D_Array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cin >> rows >> cols;
+
+    int arr[10][10];
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
