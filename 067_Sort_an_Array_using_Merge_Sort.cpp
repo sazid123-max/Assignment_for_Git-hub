155dde7ed608374d5dec2f508fa845d5622bfb07
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-29 14:34:54 +0600

    solve to Sort an Array using Merge Sort

---
+++ 067_Sort_an_Array_using_Merge_Sort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+void mergeArray(int arr[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int a[n1], b[n2];
+    for (int i = 0; i < n1; i++)
+        a[i] = arr[l + i];
+    for (int i = 0; i < n2; i++)
+        b[i] = arr[m + 1 + i];
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (a[i] <= b[j]) {
+            arr[k] = a[i];
+            i++;
+        } else {
+            arr[k] = b[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = a[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = b[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = (l + r) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        mergeArray(arr, l, m, r);
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
