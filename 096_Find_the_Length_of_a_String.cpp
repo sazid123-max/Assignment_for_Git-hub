55a3b98a478768b85ed3ddfb7c9eea846c251611
sazid123-max <sazidahammad.bd@gmail.com>
2025-11-30 22:56:12 +0600

    solve Find the Length of a String

---
+++ 096_Find_the_Length_of_a_String.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    cout << "Length of the string = " << str.length();
+    return 0;
+}
+
